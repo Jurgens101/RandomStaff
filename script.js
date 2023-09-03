2317f409f951e2a75271051419fb35f6d558137d
jurgens101 <jurgensftw@gmail.com>
2023-09-04 01:16:26 +0200

    add new boxes

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function neo(){
+function plus(){
     const x=document.createElement("div");
-    document.body.appendChild(x)
+    document.body.appendChild(x);
  }
+
+function push(){
+    const y=document.createElement("span");
+    document.body.appendChild(y);
+}
+
+

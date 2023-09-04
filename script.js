c740cde825505e29c391b7822376e38b51116113
jurgens101 <jurgensftw@gmail.com>
2023-09-04 15:49:18 +0200

    create click game

--- script.js
+++ script.js
@@ -8,4 +8,17 @@ function push(){
     document.body.appendChild(y);
 }
 
+function col1(z)
+{
+    z.style.backgroundColor = "red";
+}
 
+function col2(w)
+{
+    var q=w.style.backgroundColor;
+    if (w.style.backgroundColor=="red"){
+        w.style.backgroundColor="green";
+    }else{
+            w.style.backgroundColor="red";
+        }
+    }

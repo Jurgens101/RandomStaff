53ccf3a64ea366faa219cf9b5a61ca5216bc765a
jurgens101 <jurgensftw@gmail.com>
2023-09-04 15:50:21 +0200

    update js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function plus(){
-    const x=document.createElement("div");
-    document.body.appendChild(x);
- }
-
-function push(){
-    const y=document.createElement("span");
-    document.body.appendChild(y);
-}
 
 function col1(z)
 {
